c021fea8d906258c4ec0e67b3e28bc9a8d46b553
Tom Rees <tomrees@amazon.com>
2017-10-23 09:45:29 +0100

    Update path

--- selectProjectWorkspace.py
+++ selectProjectWorkspace.py
@@ -6,7 +6,7 @@ import sys
 import i3
 import newProjectWorkspaces
 
-subprocess.Popen(['xterm', '-e', 'i3-scripts/listProjectWorkspaces.py | /home/local/ANT/tomrees/.fzf/bin/fzf | xargs -0 i3-scripts/newProjectWorkspaces.py'])
+subprocess.Popen(['xterm', '-e', '/home/local/ANT/tomrees/code/i3-scripts/listProjectWorkspaces.py | /home/local/ANT/tomrees/.fzf/bin/fzf | xargs -0 /home/local/ANT/tomrees/i3-scripts/newProjectWorkspaces.py'])
 
 names = list(set(map(lambda x: x['name'].split("-")[0], i3.get_workspaces())))
 for name in names:
