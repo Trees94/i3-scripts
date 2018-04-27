9c81e08dc158804c3d1f789e88be36a82789ac63
Tom Rees <tomrees@amazon.com>
2018-04-27 14:40:47 +0100

    Use better config for dirs

--- selectProjectWorkspace.py
+++ selectProjectWorkspace.py
@@ -5,8 +5,9 @@ import subprocess
 import sys
 import i3
 import newProjectWorkspaces
+import constants
 
-subprocess.Popen(['xterm', '-e', '/home/local/ANT/tomrees/code/i3-scripts/listProjectWorkspaces.py | /home/local/ANT/tomrees/.fzf/bin/fzf | xargs -0 /home/local/ANT/tomrees/code/i3-scripts/newProjectWorkspaces.py'])
+subprocess.Popen(['xterm', '-e', constants.SCRIPT_DIR + 'listProjectWorkspaces.py | ' + constants.HOME_DIR + '.fzf/bin/fzf | xargs -0 ' + constants.SCRIPT_DIR + 'newProjectWorkspaces.py'])
 
 names = list(set(map(lambda x: x['name'].split("-")[0], i3.get_workspaces())))
 for name in names:
