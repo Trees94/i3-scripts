5882b8ddc72323acedb6047d70eb3aabb59caaac
Tom Rees <tomrees@amazon.com>
2019-08-05 11:31:17 +0100

    Fixes

--- watchLogins.sh
+++ watchLogins.sh
@@ -3,6 +3,6 @@
 dbus-monitor --session "type=signal,interface=org.gnome.ScreenSaver,member=ActiveChanged" \
 | while read line; do 
   if [ x"$(echo "$line" | grep 'boolean false')" != x ] ; then 
-	  $HOME/bin/startWorkOn $(/home/local/ANT/tomrees/code/i3-scripts/currentWorkspace.py); 
+	  $HOME/bin/startWorkOn $($HOME/programs/i3-scripts/currentWorkspace.py); 
   fi; 
   done
